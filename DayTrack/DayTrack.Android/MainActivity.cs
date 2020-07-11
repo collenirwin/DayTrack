518cec33d9e340f3c20c16fe579d678a4206a8d4
collenirwin <collenirwin@gmail.com>
2020-07-11 09:45:26 -0400

    update android manifest, add icons

--- DayTrack/DayTrack.Android/MainActivity.cs
+++ DayTrack/DayTrack.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace DayTrack.Droid
 {
-    [Activity(Label = "DayTrack", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Day Tracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
