76833d3d14078b0c3dc067e9bc622119b560244e
collenirwin <collenirwin@gmail.com>
2020-07-13 10:02:21 -0400

    add docstrings to TrackerLogService

--- DayTrack/DayTrack/Services/TrackerLogService.cs
+++ DayTrack/DayTrack/Services/TrackerLogService.cs
@@ -11,8 +11,14 @@ using System.Threading.Tasks;
 
 namespace DayTrack.Services
 {
+    /// <summary>
+    /// Contains methods for interaction with the <see cref="LoggedDay"/> table.
+    /// </summary>
     public class TrackerLogService
     {
+        /// <summary>
+        /// Available sort options for <see cref="TryGetAllLoggedDayGroupsAsync(int, GroupSortOption)"/> results.
+        /// </summary>
         public enum GroupSortOption
         {
             DateDescending,
@@ -40,6 +46,12 @@ namespace DayTrack.Services
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Adds a new <see cref="LoggedDay"/> to the database with the given date and under the given tracker.
+        /// </summary>
+        /// <param name="day">Date to log.</param>
+        /// <param name="trackerId">Id of the tracker to log this under.</param>
+        /// <returns>true if successful.</returns>
         public async Task<bool> TryLogDayAsync(DateTime day, int trackerId) =>
             await Try.RunAsync(async () => await LogDayAsync(day, trackerId),
                 ex => _logger.Error(ex, $"Failed to log day (trackerId: {trackerId}, day: {day})."));
@@ -51,6 +63,11 @@ namespace DayTrack.Services
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Deletes the <see cref="LoggedDay"/> with the given id.
+        /// </summary>
+        /// <param name="id">Id of the logged day to delete.</param>
+        /// <returns>true if successful.</returns>
         public async Task<bool> TryDeleteLoggedDayAsync(int id) =>
             await Try.RunAsync(async () => await DeleteLoggedDayAsync(id),
                 ex => _logger.Error(ex, $"Failed to delete logged day with id {id}."));
@@ -61,6 +78,11 @@ namespace DayTrack.Services
                 .OrderByDescending(day => day.Date)
                 .ToListAsync();
 
+        /// <summary>
+        /// Gets all <see cref="LoggedDay"/> with the given tracker id, in descending order by date.
+        /// </summary>
+        /// <param name="trackerId">Tracker id of the logged days to get.</param>
+        /// <returns>All logged days under the given tracker.</returns>
         public async Task<IEnumerable<LoggedDay>> TryGetAllLoggedDaysAsync(int trackerId) =>
             await Try.RunAsync(async () => await GetAllLoggedDaysAsync(trackerId),
                 ex => _logger.Error(ex, $"Failed to get all logged days for tracker id {trackerId}."));
@@ -88,6 +110,12 @@ namespace DayTrack.Services
             return await query.ToListAsync();
         }
 
+        /// <summary>
+        /// Gets all <see cref="LoggedDay"/>s as <see cref="LoggedDayGroup"/>s for a given tracker id.
+        /// </summary>
+        /// <param name="trackerId">Tracker id of the logged days to get.</param>
+        /// <param name="sortOption">Specifies the way to sort the results.</param>
+        /// <returns>All logged day groups under the given tracker, in the given sort order.</returns>
         public async Task<IEnumerable<LoggedDayGroup>> TryGetAllLoggedDayGroupsAsync(int trackerId,
             GroupSortOption sortOption) =>
                 await Try.RunAsync(async () => await GetAllLoggedDayGroupsAsync(trackerId, sortOption),
@@ -104,6 +132,12 @@ namespace DayTrack.Services
             await _context.BulkInsertAsync(loggedDays);
         }
 
+        /// <summary>
+        /// Adds new <see cref="LoggedDay"/>s to the database with the given dates and under the given tracker.
+        /// </summary>
+        /// <param name="day">Dates to log.</param>
+        /// <param name="trackerId">Id of the tracker to log these under.</param>
+        /// <returns>true if successful.</returns>
         public async Task<bool> TryBulkAddEntriesAsync(IEnumerable<DateTime> days, int trackerId) =>
             await Try.RunAsync(async () => await BulkAddEntriesAsync(days, trackerId),
                 ex => _logger.Error(ex, $"Bulk insert operation failed (tracker id: {trackerId})."));
