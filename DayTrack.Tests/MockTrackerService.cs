5ce125311c2a11e355ed9486c1a9f721c343cb9f
collenirwin <collenirwin@gmail.com>
2020-07-20 13:21:05 -0400

    fully impl MockTrackerService

--- DayTrack.Tests/MockTrackerService.cs
+++ DayTrack.Tests/MockTrackerService.cs
@@ -1,42 +1,55 @@
 ﻿using DayTrack.Models;
 using DayTrack.Services;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DayTrack.Tests
 {
     public class MockTrackerService : ITrackerService
     {
-        public Task<Tracker> TryAddTrackerAsync(string name) =>
-            Task.FromResult(new Tracker
+        private readonly List<Tracker> _trackers = new List<Tracker>();
+        private int _idCount = 1;
+
+        public Task<Tracker> TryAddTrackerAsync(string name)
+        {
+            var tracker = new Tracker
             {
-                Id = 0,
+                Id = _idCount++,
                 Name = name
-            });
+            };
 
-        public Task<bool> TryDeleteTrackerAsync(int id) => Task.FromResult(true);
+            _trackers.Add(tracker);
+            return Task.FromResult(tracker);
+        }
 
-        public Task<IEnumerable<Tracker>> TryGetAllTrackersAsync() =>
-            Task.FromResult(new[]
+
+        public Task<bool> TryDeleteTrackerAsync(int id)
+        {
+            var tracker = _trackers.FirstOrDefault(t => t.Id == id);
+
+            if (tracker is null)
             {
-                new Tracker
-                {
-                    Id = 0,
-                    Name = "1"
-                },
-                new Tracker
-                {
-                    Id = 1,
-                    Name = "2"
-                }
-            }.AsEnumerable());
+                return Task.FromResult(false);
+            }
 
-        public Task<Tracker> TryUpdateTrackerNameAsync(int id, string name) =>
-            Task.FromResult(new Tracker
+            _trackers.Remove(tracker);
+            return Task.FromResult(true);
+        }
+
+        public Task<IEnumerable<Tracker>> TryGetAllTrackersAsync() => Task.FromResult(_trackers.AsEnumerable());
+
+        public Task<Tracker> TryUpdateTrackerNameAsync(int id, string name)
+        {
+            var tracker = _trackers.FirstOrDefault(t => t.Id == id);
+
+            if (tracker is null)
             {
-                Id = id,
-                Name = name
-            });
+                return null;
+            }
 
+            tracker.Name = name;
+            return Task.FromResult(tracker);
+        }
     }
 }
