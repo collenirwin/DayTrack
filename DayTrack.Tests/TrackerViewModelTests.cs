156604629aac4cbf195c69365e7908c24e79e13c
collenirwin <collenirwin@gmail.com>
2020-07-20 13:26:25 -0400

    add more tests for TrackerViewModel.CreateAsync

--- DayTrack.Tests/TrackerViewModelTests.cs
+++ DayTrack.Tests/TrackerViewModelTests.cs
@@ -1,5 +1,7 @@
-﻿using DayTrack.ViewModels;
+﻿using DayTrack.Models;
+using DayTrack.ViewModels;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 using Xunit;
 
 namespace DayTrack.Tests
@@ -23,6 +25,62 @@ namespace DayTrack.Tests
 
             // assert
             Assert.True(vm.HasError);
+        }
+
+        [Theory]
+        [InlineData("Collen")]
+        [InlineData(" Col len ")]
+        public async Task CreateAsync_WithName_HasNoError(string name)
+        {
+            // arrange
+            var vm = new TrackerViewModel(new MockTrackerService())
+            {
+                Name = name
+            };
+
+            // act
+            await vm.CreateAsync();
+
+            // assert
+            Assert.False(vm.HasError);
+        }
+
+        [Fact]
+        public async Task CreateAsync_WithName_AddsToAllTrackers()
+        {
+            // arrange
+            string name = "Collen";
+            var vm = new TrackerViewModel(new MockTrackerService())
+            {
+                Name = name
+            };
+
+            // act
+            await vm.CreateAsync();
+
+            // assert
+            Assert.Contains(vm.AllTrackers, tracker => tracker.Name == name);
+        }
+
+        [Fact]
+        public async Task CreateAsync_WithName_SendsMessageWithTracker()
+        {
+            // arrange
+            string name = "Collen";
+            var vm = new TrackerViewModel(new MockTrackerService())
+            {
+                Name = name
+            };
+
+            Tracker createdTracker = null;
+            MessagingCenter.Subscribe<TrackerViewModel, Tracker>(this, nameof(vm.CreateCommand),
+                (sender, tracker) => createdTracker = tracker);
+
+            // act
+            await vm.CreateAsync();
+
+            // assert
+            Assert.Equal(name, createdTracker?.Name);
         }
     }
 }
