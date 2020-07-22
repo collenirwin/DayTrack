5c9fce5bd14d88693758e58e66598eb83c5d284f
collenirwin <collenirwin@gmail.com>
2020-07-21 21:13:30 -0400

    add tests for TrackerLogViewModel.LogDayAsync

---
+++ DayTrack.Tests/TrackerLogViewModelTests.cs
@@ -0,0 +1,51 @@
+﻿using DayTrack.Models;
+using DayTrack.Tests.Mocks;
+using DayTrack.ViewModels;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xunit;
+
+namespace DayTrack.Tests
+{
+    /// <summary>
+    /// Tests for <see cref="TrackerLogViewModel"/> methods.
+    /// </summary>
+    public class TrackerLogViewModelTests
+    {
+        #region LogDayAsync
+
+        [Fact]
+        public async Task LogDayAsync_AddsToAllDayGroups()
+        {
+            // arrange
+            var tracker = new Tracker { Id = 0 };
+            var vm = new TrackerLogViewModel(tracker, new MockTrackerLogService());
+
+            // act
+            await vm.LogDayAsync();
+
+            // assert
+            Assert.Single(vm.AllDayGroups, group => group.Date == vm.DateToLog);
+        }
+
+        [Fact]
+        public async Task LogDayAsync_ServiceFailure_SendsMessage()
+        {
+            // arrange
+            var tracker = new Tracker();
+            var vm = new TrackerLogViewModel(tracker, new FailingTrackerLogService());
+
+            bool messageSent = false;
+            MessagingCenter.Subscribe<TrackerLogViewModel>(this, TrackerLogViewModel.DatabaseErrorMessage,
+                sender => messageSent = true);
+
+            // act
+            await vm.LogDayAsync();
+
+            // assert
+            Assert.True(messageSent);
+        }
+
+        #endregion
+    }
+}
