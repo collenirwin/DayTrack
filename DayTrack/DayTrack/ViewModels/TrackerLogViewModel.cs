bb492528e1642631ebb94fe51c58f9a3d3a420c7
collenirwin <collenirwin@gmail.com>
2020-07-25 23:08:07 -0400

    add docstrings to TrackerLogViewModel

--- DayTrack/DayTrack/ViewModels/TrackerLogViewModel.cs
+++ DayTrack/DayTrack/ViewModels/TrackerLogViewModel.cs
@@ -11,6 +11,9 @@ using Xamarin.Forms;
 [assembly: InternalsVisibleTo("DayTrack.Tests")]
 namespace DayTrack.ViewModels
 {
+    /// <summary>
+    /// Provides an interface for interaction with <see cref="LoggedDay"/>s.
+    /// </summary>
     public class TrackerLogViewModel : ViewModelBase
     {
         private DateTime _dateToLog = DateTime.Now.Date;
@@ -20,35 +23,66 @@ namespace DayTrack.ViewModels
         private readonly Tracker _tracker;
         private readonly ITrackerLogService _logService;
 
+        /// <summary>
+        /// Message to subscribe to for notification of database errors during command execution.
+        /// </summary>
         public const string DatabaseErrorMessage = "DatabaseError";
 
+        /// <summary>
+        /// Date for a new <see cref="LoggedDay"/>.
+        /// </summary>
         public DateTime DateToLog
         {
             get => _dateToLog;
             set => SetAndRaiseIfChanged(ref _dateToLog, value);
         }
 
+        /// <summary>
+        /// Selected sort option index (converts directly to a <see cref="GroupSortOption"/> value).
+        /// </summary>
         public int SortOptionIndex
         {
             get => (int)_sortOption;
             set => SetAndRaiseIfChanged(ref _sortOption, (GroupSortOption)value);
         }
 
+        /// <summary>
+        /// All <see cref="LoggedDay"/>s from the database.
+        /// </summary>
         public ObservableCollection<LoggedDay> AllDays
         {
             get => _allDays;
             set => SetAndRaiseIfChanged(ref _allDays, value);
         }
 
+        /// <summary>
+        /// All <see cref="LoggedDay"/>s represented as <see cref="LoggedDayGroup"/>s.
+        /// </summary>
         public ObservableCollection<LoggedDayGroup> AllDayGroups
         {
             get => _allDayGroups;
             set => SetAndRaiseIfChanged(ref _allDayGroups, value);
         }
 
+        /// <summary>
+        /// Create a new <see cref="LoggedDay"/> with the current <see cref="DateToLog"/>.
+        /// </summary>
         public ICommand LogDayCommand { get; }
+
+        /// <summary>
+        /// Delete a logged day (requires the <see cref="LoggedDay"/> to delete).
+        /// </summary>
         public ICommand DeleteLoggedDayCommand { get; }
+
+        /// <summary>
+        /// Get all <see cref="LoggedDay"/>s from the database, storing them in <see cref="AllDays"/>.
+        /// </summary>
         public ICommand PullAllDaysCommand { get; }
+
+        /// <summary>
+        /// Get all <see cref="LoggedDay"/>s from the database as <see cref="LoggedDayGroup"/>s,
+        /// storing them in <see cref="AllDayGroups"/>.
+        /// </summary>
         public ICommand PullAllDayGroupsCommand { get; }
 
         public TrackerLogViewModel(Tracker tracker, ITrackerLogService logService)
