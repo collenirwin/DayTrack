850a70114530ce3a0f6d2dd137f10ab5bd2b4663
collenirwin <collenirwin@gmail.com>
2020-07-27 17:42:46 -0400

    add TrackerStatsPage

---
+++ DayTrack/DayTrack/Views/TrackerStatsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DayTrack.Models;
+using DayTrack.ViewModels;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DayTrack.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TrackerStatsPage : ContentPage
+    {
+        private readonly TrackerLogViewModel _viewModel;
+
+        public TrackerStatsPage()
+        {
+            InitializeComponent();
+        }
+
+        public TrackerStatsPage(Tracker tracker, TrackerLogViewModel trackerLogViewModel) : this()
+        {
+            Title = $"{tracker.Name} stats";
+            BindingContext = _viewModel = trackerLogViewModel;
+        }
+
+        private async void OnAppearing(object sender, EventArgs e) =>
+            await Task.Run(() => _viewModel.PullStatsCommand.Execute(null));
+    }
+}
