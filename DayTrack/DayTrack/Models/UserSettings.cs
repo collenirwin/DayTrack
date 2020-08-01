c9c1e97ddf16fac059de7896038d492a629dbb0a
collenirwin <collenirwin@gmail.com>
2020-08-01 01:57:38 -0400

    add UserSettings model, DateFormats utility

---
+++ DayTrack/DayTrack/Models/UserSettings.cs
@@ -0,0 +1,15 @@
+﻿using DayTrack.Utils;
+
+namespace DayTrack.Models
+{
+    /// <summary>
+    /// Contains user-chosen application settings.
+    /// </summary>
+    public class UserSettings
+    {
+        /// <summary>
+        /// Date display format (defaults to <see cref="DateFormats.ShortYearMonthDay"/>).
+        /// </summary>
+        public string DateFormat { get; set; } = DateFormats.ShortYearMonthDay;
+    }
+}
