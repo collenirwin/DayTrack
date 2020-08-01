b536301eaef6007ed283d4a900a1e29fee5cf543
collenirwin <collenirwin@gmail.com>
2020-08-01 02:49:56 -0400

    add SettingsViewModel

---
+++ DayTrack/DayTrack/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,66 @@
+﻿using DayTrack.Services;
+using DayTrack.Utils;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DayTrack.ViewModels
+{
+    /// <summary>
+    /// Provides an interface for managing application settings.
+    /// </summary>
+    public class SettingsViewModel : ViewModelBase
+    {
+        private string _dateFormat;
+        private readonly SettingsService _settingsService;
+
+        /// <summary>
+        /// The current date format setting.
+        /// </summary>
+        public string DateFormat
+        {
+            get => _dateFormat;
+            set
+            {
+                if (_dateFormat != value && AllDateFormats.Contains(value))
+                {
+                    SetAndRaiseIfChanged(ref _dateFormat, value);
+                    SaveUserSettingsCommand.Execute(null);
+                }
+            }
+        }
+
+        /// <summary>
+        /// All available date format settings.
+        /// </summary>
+        public string[] AllDateFormats { get; } = new[]
+        {
+            DateFormats.ShortYearMonthDay,
+            DateFormats.ShortDayMonthYear,
+            DateFormats.ShortMonthDayYear,
+            DateFormats.LongYearMonthDay,
+            DateFormats.LongDayMonthYear,
+            DateFormats.LongMonthDayYear
+        };
+
+        public ICommand SaveUserSettingsCommand { get; }
+
+        public SettingsViewModel(SettingsService settingsService)
+        {
+            _settingsService = settingsService;
+            _dateFormat = settingsService.UserSettings.DateFormat;
+
+            SaveUserSettingsCommand = new Command(() => SaveUserSettings());
+        }
+
+        internal void SaveUserSettings()
+        {
+            bool successful = _settingsService.TrySaveUserSettings();
+
+            if (!successful)
+            {
+                MessagingCenter.Send(this, nameof(SaveUserSettingsCommand));
+            }
+        }
+    }
+}
