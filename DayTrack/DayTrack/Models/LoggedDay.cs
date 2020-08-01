250281091ea3d4e2a536835fe91b4359c3d79533
collenirwin <collenirwin@gmail.com>
2020-07-31 21:00:31 -0400

    prep models for new sqlite ORM

--- DayTrack/DayTrack/Models/LoggedDay.cs
+++ DayTrack/DayTrack/Models/LoggedDay.cs
@@ -1,18 +1,22 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using TableAttribute = SQLite.TableAttribute;
 
 namespace DayTrack.Models
 {
     /// <summary>
     /// Represents a day logged under a <see cref="Tracker"/>.
     /// </summary>
+    [Table("LoggedDays")]
     public class LoggedDay : ITimeStamped
     {
         /// <summary>
         /// Auto-generated primary key.
         /// </summary>
         [Key]
+        [PrimaryKey, AutoIncrement]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
@@ -20,6 +24,7 @@ namespace DayTrack.Models
         /// Id of the <see cref="Tracker"/> this day is logged under.
         /// </summary>
         [ForeignKey(nameof(Tracker))]
+        [Indexed]
         public int TrackerId { get; set; }
 
         /// <summary>
--- DayTrack/DayTrack/Models/Tracker.cs
+++ DayTrack/DayTrack/Models/Tracker.cs
@@ -1,19 +1,23 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using TableAttribute = SQLite.TableAttribute;
 
 namespace DayTrack.Models
 {
     /// <summary>
     /// Represents a tracker the user can log days under.
     /// </summary>
+    [Table("Trackers")]
     public class Tracker : ITimeStamped
     {
         /// <summary>
         /// Auto-generated primary key.
         /// </summary>
         [Key]
+        [PrimaryKey, AutoIncrement]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
